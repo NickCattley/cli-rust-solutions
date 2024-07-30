ebb1fb7f469b81a8b960d485b8c498e3e5af3342
Nicholas Cattley <nick@nick-pc.localdomain>
2024-07-30 07:39:59 +0100

    Progress on Chapter 2.

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("echor")
+        .version("0.1.0")
+        .author("Nick Cattley")
+        .about("Rust Echo")
+        .arg(
+            Arg::with_name("text")
+                .value_name("TEXT")
+                .help("Input text")
+                .required(true)
+                .min_values(1),
+        )
+        .arg(
+            Arg::with_name("omit_newline")
+                .short("n")
+                .help("Do not print new line")
+                .takes_value(false),
+        )
+        .get_matches();
+
+    println!("{:?}", matches);
 }
